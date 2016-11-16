7f863b3f97ebe14ea306a841981165c8f575ecd1
e-digitalis <e-digitalis@users.noreply.github.com>
2016-11-16 21:27:15 +0100

    Update CategoryAdmin.java

--- src/groupworkNamaste/CategoryAdmin.java
+++ src/groupworkNamaste/CategoryAdmin.java
@@ -1,12 +1,17 @@
 package groupworkNamaste;
 
 public class CategoryAdmin extends SuperClassEmployee {
+	//One of the five employee category subclasses 
 
+	//these two variables keep track of how many employees belong to each category
 	private static int counterAdmin = 0;
-	private int adminId;
+	private int adminId; 
 
+	//people that work in administration receive a fixed, not a variable, bonus
 	private int fixedValueBonus = 100;
 
+	//for admin we need to register only the four variables that the superclass needs
+	//since the bonus is fixed value there is no need for extra input for the bonus calculation method
 	public CategoryAdmin(String name, double salary, int yearOfBirth, EnumCategory category) {
 		super(name, salary, yearOfBirth, category);
 		counterAdmin++;
