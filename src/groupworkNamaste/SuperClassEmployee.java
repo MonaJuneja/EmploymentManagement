d74de7394b0a6c10c1189bbb36d532066856fd6f
e-digitalis <e-digitalis@users.noreply.github.com>
2016-11-16 21:54:26 +0100

    Update SuperClassEmployee.java

--- src/groupworkNamaste/SuperClassEmployee.java
+++ src/groupworkNamaste/SuperClassEmployee.java
@@ -2,15 +2,21 @@ package groupworkNamaste;
 
 public abstract class SuperClassEmployee {
 
+	//variables used to keep track of employees in personnel system
 	private String name;
 	private double salary;
 	private int yearOfBirth;
 	private EnumCategory category;
 	private int employmentId;
+	//tracker to keep track of number of employees
 	private static int counterId = 0;
+	//variable used for the bonus calculations
 	private double bonus;
 	
 	//CONSTRUCTOR
+	//all these variables are common to all personnel categories
+	//user needs to input all but counter
+	//data will be handled by super class as indicated in the constructor
 	public SuperClassEmployee(String name, double salary, int yearOfBirth, EnumCategory category) {
 
 		counterId++;
@@ -24,13 +30,13 @@ public abstract class SuperClassEmployee {
 	//ABSTRACT METHOD - enforcing every subclass to implement this method 
 	public abstract void bonusCalculation();
 	
-	
+	//overriding toString method
 	@Override
 	public String toString() {
 		return "[name=" + name + ", salary=" + salary + ", yearOfBirth=" + yearOfBirth
 				+ ", category=" + category + ", employmentId=" + employmentId + "]";
 	}
-	//TODO: få fram employment ID
+	//TODO: fÃ¥ fram employment ID
 	
 	//GETTERS AND SETTERS
 	public EnumCategory getCategory() {
