03d84b5ffed2da18b1d7d2a14140762fc6f30d36
e-digitalis <e-digitalis@users.noreply.github.com>
2016-11-16 21:47:13 +0100

    Update CategoryAdmin.java

--- src/groupworkNamaste/CategoryAdmin.java
+++ src/groupworkNamaste/CategoryAdmin.java
@@ -12,16 +12,17 @@ public class CategoryAdmin extends SuperClassEmployee {
 
 	//for admin we need to register only the four variables that the superclass needs
 	//since the bonus is fixed value there is no need for extra input for the bonus calculation method
+	//the counter is tracked automatically - no user input necessary
 	public CategoryAdmin(String name, double salary, int yearOfBirth, EnumCategory category) {
 		super(name, salary, yearOfBirth, category);
 		counterAdmin++;
 		this.adminId = counterAdmin;
 	}
 
-	//this method overrides the abstract method from the superclass
-	@Override
+	@Override	//this method overrides the abstract method from the superclass
 	public void bonusCalculation() {
 		setBonus(100);	
+		//setBonus is used to change the private variable in the super class
 	}
 	
 	//this method is used when we need the number of personnel in the admin category
