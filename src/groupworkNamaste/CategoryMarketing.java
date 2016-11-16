106b048f18f253eddafecb81a1a2dd7d623537e2
e-digitalis <e-digitalis@users.noreply.github.com>
2016-11-16 21:47:37 +0100

    Update CategoryMarketing.java

--- src/groupworkNamaste/CategoryMarketing.java
+++ src/groupworkNamaste/CategoryMarketing.java
@@ -2,11 +2,15 @@ package groupworkNamaste;
 
 public class CategoryMarketing extends SuperClassEmployee {
 		//One of the five employee category subclasses 
-
+	
+	//these two variables keep track of how many employees belong to each category
 	private static int counterMkt = 0;
 	private int mktId;
 	private int noOfNewCustomers;
+	//last variable is used for bonus calculation
 
+	//we register all variables that the superclass needs + one additional variable for the bonus calculation
+	//the counter is tracked automatically - no user input necessary
 	public CategoryMarketing(String name, double salary, int yearOfBirth, EnumCategory category, int noOfNewCustomers) {
 		super(name, salary, yearOfBirth, category);
 		this.noOfNewCustomers = noOfNewCustomers;
@@ -15,9 +19,10 @@ public class CategoryMarketing extends SuperClassEmployee {
 
 	}
 
-	@Override
+	@Override	//this method overrides the abstract method from the superclass
 	public void bonusCalculation() {
 		setBonus(noOfNewCustomers * 20);
+		//setBonus is used to change the private variable in the super class
 		}
 
 	public int getNoOfNewCustomers() {
@@ -27,7 +32,7 @@ public class CategoryMarketing extends SuperClassEmployee {
 	public void setNoOfNewCustomers(int noOfNewCustomers) {
 		this.noOfNewCustomers = noOfNewCustomers;
 	}
-
+	//this method is used when we need the number of personnel in this category
 	public static int getCounterMkt() {
 		return counterMkt;
 	}
